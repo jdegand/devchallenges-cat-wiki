50bf7cb99d15e29d0c8f6fcd5a495dbf38e16b82
jdegand <70610011+jdegand@users.noreply.github.com>
2023-12-05 15:56:33 -0500

    docs: Suspense issues in breed component

--- app/breeds/breed/page.js
+++ app/breeds/breed/page.js
@@ -32,7 +32,7 @@ import Image from 'next/legacy/image';
 
 import { useState, useEffect, Suspense } from 'react';
 
-export default function Breed2() {
+function Breed2() {
 
   const [json, setJson] = useState(null);
   const [image, setImage] = useState(null);
@@ -49,6 +49,7 @@ export default function Breed2() {
 
     document.title = `${searchTerm} details`;
 
+    // Maybe a CORS issue with some requests
     async function getBreedBySearchTerm(searchTerm) {
       const res = await fetch(`https://api.thecatapi.com/v1/breeds/search?q=${searchTerm}`, {
         method: 'GET',
@@ -56,6 +57,10 @@ export default function Breed2() {
           "x-api-key": process.env.API_KEY
         }
       })
+
+      if (!res.ok) {
+        throw new Error("API request for breed failed");
+      }
 
       const json = await res.json();
       return json;
@@ -68,6 +73,11 @@ export default function Breed2() {
           "x-api-key": process.env.API_KEY
         }
       })
+
+      if (!res2.ok) {
+        throw new Error("API Request for main image failed");
+      }
+
       const image = await res2.json();
       return image;
     }
@@ -79,6 +89,11 @@ export default function Breed2() {
           "x-api-key": process.env.API_KEY
         }
       });
+
+      if (!res3.ok) {
+        throw new Error("API Request for other images failed");
+      }
+
       const otherImages = await res3.json();
       return otherImages;
     }
@@ -106,40 +121,66 @@ export default function Breed2() {
   }, [searchTerm])
 
   return (
-    <main>
-      <Suspense fallback={<h1>Loading</h1>}>
-        <div className={styles.topGrid}>
-          <div>
-            {image && <Image src={image.url} alt="" width="400" height="400" className={styles.radius} priority />}
-          </div>
-          {json && <section>
-            <h1>{json[0].name}</h1>
-            <p className={styles.description}>{json[0].description}</p>
-            <h2>Termperament: {json[0].temperament}</h2>
-            <h2>Origin: {json[0].origin}</h2>
-            <h2>Life Span: {json[0].life_span} years</h2>
-            <h2 className={styles.bubbleGrid}>Adaptability: <Bubble count={json && json[0].adaptability} /></h2>
-            <h2 className={styles.bubbleGrid}>Affection Level: <Bubble count={json && json[0].affection_level} /></h2>
-            <h2 className={styles.bubbleGrid}>Child Friendly: <Bubble count={json && json[0].child_friendly} /></h2>
-            <h2 className={styles.bubbleGrid}>Grooming: <Bubble count={json && json[0].grooming} /></h2>
-            <h2 className={styles.bubbleGrid}>Intelligence: <Bubble count={json && json[0].intelligence} /></h2>
-            <h2 className={styles.bubbleGrid}>Health Issues: <Bubble count={json && json[0].health_issues} /></h2>
-            <h2 className={styles.bubbleGrid}>Social Needs: <Bubble count={json && json[0].social_needs} /></h2>
-            <h2 className={styles.bubbleGrid}>Stranger Friendly: <Bubble count={json && json[0].stranger_friendly} /></h2>
-          </section>}
+    <section>
+      <div className={styles.topGrid}>
+        <div>
+          {image && <Image src={image.url} alt="" width="400" height="400" className={styles.radius} priority />}
         </div>
-        {otherImageUrls && <section>
-          <h3 className={styles.fs35}>Other Images</h3>
-          <div className={styles.otherImagesGrid}>
-            {
-              otherImageUrls.map((image, index) => {
-                return (
-                  <Image key={index} src={image.url} alt="" width="400" height="400" objectFit="cover" className={styles.radius} />
-                )
-              })
-            }
-          </div>
+        {json && <section>
+          <h1>{json[0].name}</h1>
+          <p className={styles.description}>{json[0].description}</p>
+          <h2>Termperament: {json[0].temperament}</h2>
+          <h2>Origin: {json[0].origin}</h2>
+          <h2>Life Span: {json[0].life_span} years</h2>
+          <h2 className={styles.bubbleGrid}>Adaptability: <Bubble count={json && json[0].adaptability} /></h2>
+          <h2 className={styles.bubbleGrid}>Affection Level: <Bubble count={json && json[0].affection_level} /></h2>
+          <h2 className={styles.bubbleGrid}>Child Friendly: <Bubble count={json && json[0].child_friendly} /></h2>
+          <h2 className={styles.bubbleGrid}>Grooming: <Bubble count={json && json[0].grooming} /></h2>
+          <h2 className={styles.bubbleGrid}>Intelligence: <Bubble count={json && json[0].intelligence} /></h2>
+          <h2 className={styles.bubbleGrid}>Health Issues: <Bubble count={json && json[0].health_issues} /></h2>
+          <h2 className={styles.bubbleGrid}>Social Needs: <Bubble count={json && json[0].social_needs} /></h2>
+          <h2 className={styles.bubbleGrid}>Stranger Friendly: <Bubble count={json && json[0].stranger_friendly} /></h2>
         </section>}
+      </div>
+      {otherImageUrls && <section>
+        <h3 className={styles.fs35}>Other Images</h3>
+        <div className={styles.otherImagesGrid}>
+          {
+            otherImageUrls.map((image, index) => {
+              return (
+                <Image key={index} src={image.url} alt="" width="400" height="400" objectFit="cover" className={styles.radius} />
+              )
+            })
+          }
+        </div>
+      </section>}
+    </section>
+  )
+}
+
+export default function Breed({ searchParams }) {
+
+  // The fallback doesn't show appear in the html 
+  // 'use client' and useEffect problem ?
+  // you don't get the deopted warning anymore
+  // Problem because of key issue?
+  // https://github.com/vercel/next.js/issues/46258
+  // https://github.com/vercel/next.js/discussions/50563
+
+  const params = new URLSearchParams(searchParams);
+
+  function Loading() {
+    return (
+      <section className="fallback-section">
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
+  return (
+    <main>
+      <Suspense key={params.toString()} fallback={<Loading />}>
+        <Breed2 />
       </Suspense>
     </main>
   )
